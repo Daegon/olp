d31555814f5fcd5cdc42cec53fe0fa6c7217b9b5
zimm <romsssa@gmail.com>
2019-01-22 08:08:26 +0200

    Add webpack

---
+++ src/AppBundle/Controller/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Attribute;
+use AppBundle\Entity\Characteristic;
+use JMS\Serializer\SerializationContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('AppBundle:index:index.html.twig');
+    }
+}
